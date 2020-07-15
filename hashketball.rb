d1e06d5947b1add1cc9d61a2b75a1c1eaea173a3
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-15 19:06:04 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -329,3 +329,35 @@ def player_with_longest_name
   end 
   longest_name
 end
+
+def winning_team
+
+  game_hash
+  array_of_brooklyn_scores = []
+  array_of_charlotte_scores = []
+
+  game_hash.each do |location, nested_hash|
+    nested_hash.each do |general_info, nested_data|
+      
+      if nested_hash[:team_name] == "Brooklyn Nets" && nested_data.class == Array 
+        nested_data.each do |nested_hash|
+          if nested_hash.class == Hash 
+            array_of_brooklyn_scores.push(nested_hash[:points])
+          end
+        end
+      elsif nested_hash[:team_name] == "Charlotte Hornets" && nested_data.class == Array 
+        nested_data.each do |nested_hash|
+          if nested_hash.class == Hash 
+            array_of_charlotte_scores.push(nested_hash[:points])
+          end
+        end
+      end
+    end
+  end
+
+  brooklyn_sum = array_of_brooklyn_scores.sum
+  charlotte_sum = array_of_charlotte_scores.sum
+
+  charlotte_sum > brooklyn_sum ? (return "Charlotte Hornets, #{charlotte_sum}") : (return "Brooklyn Nets, #{brooklyn_sum}")
+  
+  end
